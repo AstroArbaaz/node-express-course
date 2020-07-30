78c4bee992883defd4be8ce8897cacbbb28af264
Arbaaz khan <mrkhan4231@gmail.com>
2020-07-30 19:54:20 +0530

    added first get route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.MockUserData=[
+  {name:'Arbaaz', lastName:'khan'},
+  {name:'John', lastName:'whick'}
+]
+
+app.get('/', function(req, res){
+  res.json({
+    sucess: true,
+    message: 'sucessfully got users. Nice!',
+    users: MockUserData
+  })
+})
+
 app.listen(8000, function(){
   console.log("server is running on port:8000");
 });
